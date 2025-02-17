392aa15c0f2da4bca48b145b1a8cf4d270419713
Eunjae Shim <53839819+eunjaeshim@users.noreply.github.com>
2025-02-17 12:17:08 -0500

    Dataset setup

--- iris_dataset/Eunjae/dataloader.py
+++ iris_dataset/Eunjae/dataloader.py
@@ -2,3 +2,27 @@ import pandas as pd
 import numpy as np
 
 
+class IrisDataset:
+    """ Loads a synthetic augmented iris data. """
+    def __init__(
+        self,
+        path="iris_synthetic_data.csv"
+    ) :
+        self.path = path
+        self.raw_df = pd.read_csv(self.path, header=0)
+        
+        self.features = list(self.raw_df.columns[:-1])
+        self.labels = list(self.raw_df.iloc[:, -1].unique())
+        self.label_to_class = {
+            label : i for i, label in enumerate(self.labels)
+        }
+
+    @property
+    def X(self) :
+        self._X = self.raw_df.iloc[:, :-1].to_numpy()
+        return self._X
+    
+    @property
+    def y(self) :
+        self._y = self.raw_df["label"].map(self.label_to_class).to_numpy(dtype=np.int8)
+        return self._y
